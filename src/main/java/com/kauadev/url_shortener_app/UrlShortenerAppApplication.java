ad2ca29d5116c83096abf23ea4438ad2d470ee6a
21kauadev <kauantnetsoares@gmail.com>
2025-05-17 11:42:38 -0300

    add files

---
+++ src/main/java/com/kauadev/url_shortener_app/UrlShortenerAppApplication.java
@@ -0,0 +1,13 @@
+package com.kauadev.url_shortener_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauadev/url_shortener_app/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauadev.url_shortener_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
