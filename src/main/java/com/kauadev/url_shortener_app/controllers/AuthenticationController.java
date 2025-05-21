e76f212afc078f1f5186ae6900741c3bc0c927bd
21kauadev <kauantnetsoares@gmail.com>
2025-05-21 12:07:35 -0300

    feat: add AuthenticationController with all necessary dependencies and register method

---
+++ src/main/java/com/kauadev/url_shortener_app/controllers/AuthenticationController.java
@@ -0,0 +1,53 @@
+package com.kauadev.url_shortener_app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kauadev.url_shortener_app.domain.user.User;
+import com.kauadev.url_shortener_app.domain.user.UserDTO;
+import com.kauadev.url_shortener_app.domain.user.exceptions.UserAlreadyExists;
+import com.kauadev.url_shortener_app.infra.ApiResponse;
+import com.kauadev.url_shortener_app.infra.security.TokenService;
+import com.kauadev.url_shortener_app.repositories.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<User>> register(@RequestBody UserDTO data) {
+
+        if (userRepository.findByUsername(data.username()) != null) {
+            throw new UserAlreadyExists();
+        }
+
+        String hashPassword = passwordEncoder.encode(data.password());
+
+        User user = new User();
+        user.setUsername(data.username());
+        user.setPassword(hashPassword);
+        user.setRole(data.role());
+        user.setEmail(data.email());
+
+        ApiResponse<User> response = new ApiResponse<User>(HttpStatus.OK.value(), true, "Usuário cadastrado.", user);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+}
--- src/main/java/com/kauadev/url_shortener_app/infra/security/TokenService.java
+++ src/main/java/com/kauadev/url_shortener_app/infra/security/TokenService.java
@@ -35,7 +35,6 @@ public class TokenService {
                 .build()
                 .verify(token)
                 .getSubject();
-
     }
 
     private Instant genExpirationInstant() {
