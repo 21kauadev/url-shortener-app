98afd3762acca04849fab36f78895fafd44d7f06
21kauadev <kauantnetsoares@gmail.com>
2025-05-22 15:01:41 -0300

    feat: protect routes with JwtAuthFilter

---
+++ src/main/java/com/kauadev/url_shortener_app/infra/security/JwtAuthFilter.java
@@ -0,0 +1,66 @@
+package com.kauadev.url_shortener_app.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.kauadev.url_shortener_app.repositories.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String token = recoverToken(request);
+
+            if (token != null) {
+                String usernameSubject = tokenService.validateTokenAndReturnSubject(token);
+                UserDetails user = userRepository.findByUsername(usernameSubject);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(user, null,
+                        user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            resolver.resolveException(request, response, null, e);
+        }
+
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorization = request.getHeader("AUTHORIZATION");
+
+        if (authorization == null)
+            return null;
+
+        String token = authorization.replace("Bearer ", "");
+
+        return token;
+    }
+
+}
--- src/main/java/com/kauadev/url_shortener_app/infra/security/SecurityConfiguration.java
+++ src/main/java/com/kauadev/url_shortener_app/infra/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.kauadev.url_shortener_app.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,17 +11,25 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().anonymous()) // temporario
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
