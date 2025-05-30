09032e38d886d797687b5606c8466f4ad86939ef
21kauadev <kauantnetsoares@gmail.com>
2025-05-30 12:22:13 -0300

    chore: add linkDTO and requestMatchers to specific routes of link

---
+++ src/main/java/com/kauadev/url_shortener_app/domain/link/LinkDTO.java
@@ -0,0 +1,5 @@
+package com.kauadev.url_shortener_app.domain.link;
+
+public record LinkDTO(String url) {
+
+}
--- src/main/java/com/kauadev/url_shortener_app/infra/security/SecurityConfiguration.java
+++ src/main/java/com/kauadev/url_shortener_app/infra/security/SecurityConfiguration.java
@@ -28,6 +28,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/link/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/link/shorten").authenticated()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
